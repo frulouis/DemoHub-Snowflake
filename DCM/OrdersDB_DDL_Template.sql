106737b467bb2d5a122d94677eb9127078b95ce1
Fru Louis <fru.louis@gmail.com>
2024-06-02 10:37:11 -0500

    Update OrdersDB_DDL_Template.sql

--- DCM/OrdersDB_DDL_Template.sql
+++ DCM/OrdersDB_DDL_Template.sql
@@ -1,10 +1,11 @@
 --!jinja
 
 {% set environments = ['dev', 'qa', 'staging', 'prod'] %}
+{% set DATA_PRODUCT = "ORDERS" %}  
+   
+{% set BU_GEO = "US" %}
 
 {% for environment in environments %}
-
-   {% set database_name = {{ environment }}+'_' +DATA_PRODUCT+'db_' + BU_GEO %}
-   {% set s3_url = 's3://demohubpublic/data/' %}  --Public dataset used for demos. Change this if you have to. 
-
+    {% set database_name = environment + '_' + DATA_PRODUCT + 'db_' + BU_GEO %}
+    {% set s3_url = 's3://demohubpublic/data/' %}
 {% endfor %}
