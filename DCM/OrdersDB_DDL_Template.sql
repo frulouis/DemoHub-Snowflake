1b55cf78b82a08dd5dfd8de891e14bc208756f49
Fru Louis <fru.louis@gmail.com>
2024-06-02 10:39:59 -0500

    Update OrdersDB_DDL_Template.sql

--- DCM/OrdersDB_DDL_Template.sql
+++ DCM/OrdersDB_DDL_Template.sql
@@ -6,6 +6,7 @@
 {% set BU_GEO = "US" %}
 
 {% for environment in environments %}
-
+    {% set database_name = environment + '_' + DATA_PRODUCT + 'db_' + BU_GEO %}
+    CREATE OR REPLACE DATABASE {{ database_name }};
     {% set s3_url = 's3://demohubpublic/data/' %}
 {% endfor %}
