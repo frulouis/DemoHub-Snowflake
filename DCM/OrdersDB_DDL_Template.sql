7dfc0229083375804b0cbc36fdd0c12dd765ce89
Fru Louis <fru.louis@gmail.com>
2024-06-02 09:55:14 -0500

    Update OrdersDB_DDL_Template.sql

--- DCM/OrdersDB_DDL_Template.sql
+++ DCM/OrdersDB_DDL_Template.sql
@@ -14,3 +14,37 @@ CREATE OR REPLACE FILE FORMAT CSV_SCHEMA_DETECTION TYPE = CSV PARSE_HEADER = TRU
 
 CREATE OR REPLACE STAGE DEMOHUB_S3_INT URL = '{{ s3_url }}/' DIRECTORY = (ENABLE = true) COMMENT = 'DemoHub S3 datasets';
 
+-- +----------------------------------------------------+
+-- |       2. LOADING DATA WITH SCHEMATIZATION      |
+-- +----------------------------------------------------+
+
+{% for table_name in ['customer', 'device', 'sales_order', 'sales_order_item'] %}
+CREATE OR REPLACE TABLE {{ table_name }} USING TEMPLATE (SELECT ARRAY_AGG(OBJECT_CONSTRUCT(*)) FROM TABLE (INFER_SCHEMA(LOCATION => '@demohub_s3_int/orders/{{ table_name }}/', FILE_FORMAT => 'CSV_SCHEMA_DETECTION')));
+{% endfor %}
+
+-- +----------------------------------------------------+
+-- |       3. CONSTRAINTS AND REFERENTIAL INTEGRITY      |
+-- +----------------------------------------------------+
+
+-- Device table
+ALTER TABLE Device ADD PRIMARY KEY (device_id);
+
+-- Customer table
+ALTER TABLE Customer ADD PRIMARY KEY (customer_id);
+
+-- Sales_Order table
+ALTER TABLE Sales_Order ADD PRIMARY KEY (order_id);
+ALTER TABLE Sales_Order ADD CONSTRAINT fk_sales_customer FOREIGN KEY (customer_id) REFERENCES Customer(customer_id);
+
+-- Sales_Order_Item table
+ALTER TABLE Sales_Order_Item ADD PRIMARY KEY (order_item_id);
+ALTER TABLE Sales_Order_Item ADD CONSTRAINT fk_order_item_order FOREIGN KEY (order_id) REFERENCES Sales_Order(order_id);
+
+
+-- +----------------------------------------------------+
+-- |       4. COPY DATA INTO TABLE       |
+-- +----------------------------------------------------+
+
+{% for table_name in ['customer', 'device', 'sales_order', 'sales_order_item'] %}
+COPY INTO {{ table_name }} FROM '@demohub_s3_int/orders/{{ table_name }}/' FILE_FORMAT = 'CSV_SCHEMA_DETECTION' MATCH_BY_COLUMN_NAME = CASE_INSENSITIVE;
+{% endfor %}
