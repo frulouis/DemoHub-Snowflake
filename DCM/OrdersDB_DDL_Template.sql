93a83077032086ded3b4c475261b41d66fa6fb5d
Fru Louis <fru.louis@gmail.com>
2024-06-02 10:16:08 -0500

    Update OrdersDB_DDL_Template.sql

--- DCM/OrdersDB_DDL_Template.sql
+++ DCM/OrdersDB_DDL_Template.sql
@@ -1,7 +1,7 @@
 --!jinja
  
 {% set database_name = 'ordersdb_' + DEPLOYMENT_TYPE + '_' + BU_GEO %}
-{% set s3_url = 's3://' + BUCKET_NAME + '/data/' %}
+{% set s3_url = 's3://demohubpublic/data/' %}
 
 -- +----------------------------------------------------+
 -- |       1. DATABASE AND SCHEMA SETUP       |
@@ -12,7 +12,7 @@ USE DATABASE {{ database_name }};
 
 CREATE OR REPLACE FILE FORMAT CSV_SCHEMA_DETECTION TYPE = CSV PARSE_HEADER = TRUE SKIP_BLANK_LINES = TRUE TRIM_SPACE = TRUE ERROR_ON_COLUMN_COUNT_MISMATCH = FALSE;
 
-CREATE OR REPLACE STAGE DEMOHUB_S3_INT URL = '{{ s3_url }}/' DIRECTORY = (ENABLE = true) COMMENT = 'DemoHub S3 datasets';
+CREATE OR REPLACE STAGE DEMOHUB_S3_INT URL = '{{ s3_url }}' DIRECTORY = (ENABLE = true) COMMENT = 'DemoHub S3 datasets';
 
 -- +----------------------------------------------------+
 -- |       2. LOADING DATA WITH SCHEMATIZATION      |
