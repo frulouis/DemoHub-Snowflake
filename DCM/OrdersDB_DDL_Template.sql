06e7d7a35e7a04b734a69921ce0b2590ff6f73ae
Fru Louis <fru.louis@gmail.com>
2024-06-02 10:31:24 -0500

    Update OrdersDB_DDL_Template.sql

--- DCM/OrdersDB_DDL_Template.sql
+++ DCM/OrdersDB_DDL_Template.sql
@@ -5,7 +5,7 @@
 {% for environment in environments %}
 
 
-   {% set database_name = {{ environment }}+'_ordersdb_' + DEPLOYMENT_TYPE + '_' + BU_GEO %}
+   {% set database_name = {{ environment }}+'_' +DATA_PRODUCT+'db_' + BU_GEO %}
    {% set s3_url = 's3://demohubpublic/data/' %}  --Public dataset used for demos. Change this if you have to. 
    
    -- +----------------------------------------------------+
