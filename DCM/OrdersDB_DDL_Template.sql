da356080c2133ea95843ca0c7245cf63706c711c
Fru Louis <fru.louis@gmail.com>
2024-06-02 10:04:33 -0500

    Update OrdersDB_DDL_Template.sql

--- DCM/OrdersDB_DDL_Template.sql
+++ DCM/OrdersDB_DDL_Template.sql
@@ -18,7 +18,7 @@ CREATE OR REPLACE STAGE DEMOHUB_S3_INT URL = '{{ s3_url }}/' DIRECTORY = (ENABLE
 -- |       2. LOADING DATA WITH SCHEMATIZATION      |
 -- +----------------------------------------------------+
 
-tables = ['customer', 'device', 'sales_order', 'sales_order_item']
+{% set tables = ['customer', 'device', 'sales_order', 'sales_order_item'] %}
 {% for table_name in tables %}
 CREATE OR REPLACE TABLE {{ table_name }} USING TEMPLATE (SELECT ARRAY_AGG(OBJECT_CONSTRUCT(*)) FROM TABLE (INFER_SCHEMA(LOCATION => '@demohub_s3_int/orders/{{ table_name }}/', FILE_FORMAT => 'CSV_SCHEMA_DETECTION')));
 {% endfor %}
